e0418c595bdc0065ac8e9c520e1774efa5efd080
Arsen Batyuchok <arsen.b@matic.com>
2021-07-10 11:05:54 +0300

    circular

--- js/circular.js
+++ js/circular.js
@@ -3,7 +3,6 @@ import VirtualScroll from 'virtual-scroll';
 function lerp(a, b, n) {
   return (1 - n) * a + n * b;
 }
-
 class CircularScroll {
   constructor() {
     this.scroller = new VirtualScroll();
@@ -13,9 +12,15 @@ class CircularScroll {
     this.radians = 2 * Math.PI / this.projects.length;
     this.y = 0;
     this.easedY = 0;
+    // це індекс ізінга, від 1 до 0.0000n ... - чим менший, тим довшим буде
+    // інерційний рух (тобто рух вже після того як скрол відбувся), і повільнішою
+    // сама анімація
+    this.easing = 0.05;
+    // це швидкість - чим більше число, тим повільніше скролиться
+    this.speed = 10;
 
     this.scroller.on(e => {
-      this.y = e.y / 10;
+      this.y = e.y / this.speed;
     });
   }
 
@@ -29,7 +34,9 @@ class CircularScroll {
   };
 
   render() {
-    this.easedY = lerp(this.easedY, this.y, 0.05);
+    this.easedY = Math.floor(this.easedY * 100) / 100;
+    this.easedY = lerp(this.easedY, this.y, this.easing);
+
     const top = this.radius * Math.sin(this.easedY / 360);
     const left = this.radius * Math.cos(this.easedY / 360) - this.radius;
 
