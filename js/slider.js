d63b4b3ef4e82e244349b64731ff0022e602f506
sergokiko <sergokiko@gmail.com>
2021-07-13 18:46:55 +0300

    slider class

--- js/slider.js
+++ js/slider.js
@@ -2,150 +2,145 @@ import gsap, { Power2 } from 'gsap';
 
 import throttle from './utils/throttle';
 
-const slides = Array.from(document.querySelectorAll('.slide'));
-const slider = document.querySelector('.slider');
+class Slider {
+  constructor(projectId) {
+    this.slider = document.querySelectorAll('.slider')[projectId];
+    this.slides = [...this.slider.querySelectorAll('.slide')];
+    this.nextBtn = this.slider.querySelector('[data-slider="next"]');
+    this.prevBtn = this.slider.querySelector('[data-slider="prev"]');
 
-const getNextPrev = () => {
-  const activeSlide = document.querySelector('.slide.active');
-  const activeIdx = slides.indexOf(activeSlide);
-  let next, prev, nextAfterNext;
+    this.setInitialSliderPosition(10);
 
-  if (activeIdx === slides.length - 1) {
-    next = slides[0];
-  } else {
-    next = slides[activeIdx + 1];
+    const throttleClickNext = throttle(() => this.goToNextSlide(), 200);
+    const throttleClickPrev = throttle(() => this.goToPrevSlide(), 200);
+
+    this.nextBtn.addEventListener('click', throttleClickNext);
+    this.prevBtn.addEventListener('click', throttleClickPrev);
+
+    window.addEventListener('keydown', e => {
+      if(e.key === 'ArrowLeft') throttleClickPrev();
+      if(e.key === 'ArrowRight') throttleClickNext();
+    });
+
+    this.slides.forEach((s, i ) => {
+      s.addEventListener('click', () => this.onSlideClick(s, i));
+    });
   }
-  if(activeIdx === slides.length - 2){
-    nextAfterNext = slides[0];
-  } else {
-    if(activeIdx === slides.length - 1){
-      nextAfterNext = slides[1];
+
+  getNextPrev () {
+    const activeSlide = this.slider.querySelector('.active');
+    const activeIdx = this.slides.indexOf(activeSlide);
+    let next, prev, nextAfterNext;
+
+    if (activeIdx === this.slides.length - 1) {
+      next = this.slides[0];
     } else {
-      nextAfterNext = slides[activeIdx + 2];
+      next = this.slides[activeIdx + 1];
     }
-  }
-
-  if (activeIdx === 0) {
-    prev = slides[slides.length - 1];
-  } else {
-    prev = slides[activeIdx - 1];
-  }
-  return [next, prev, nextAfterNext];
-};
-
-const setInitialSliderPosition = () => {
-  const activeSlide = document.querySelector('.slide.active');
-  const activeIdx = slides.indexOf(activeSlide);
-  const [next, prev] = getNextPrev();
-
-  slides.forEach((s, i) => {
-    if (i === activeIdx) {
-      gsap.set(s, { xPercent: 0 });
-      gsap.set(s.children[0], { xPercent: ((window.innerWidth - s.children[0]?.clientWidth) / (2 * s.children[0]?.clientWidth)) * 100  });
+    if(activeIdx === this.slides.length - 2){
+      nextAfterNext = this.slides[0];
     } else {
-      if (s === prev) {
-        gsap.set(s, { xPercent: -100 });
+      if(activeIdx === this.slides.length - 1){
+        nextAfterNext = this.slides[1];
+      } else {
+        nextAfterNext = this.slides[activeIdx + 2];
+      }
+    }
+
+    if (activeIdx === 0) {
+      prev = this.slides[this.slides.length - 1];
+    } else {
+      prev = this.slides[activeIdx - 1];
+    }
+    return [next, prev, nextAfterNext];
+  };
+
+  setInitialSliderPosition (time) {
+    setTimeout(() => {
+      const activeSlide = this.slider.querySelector('.active');
+      const activeIdx = this.slides.indexOf(activeSlide);
+      const [next, prev] = this.getNextPrev();
+
+      this.slides.forEach((s, i) => {
+        if (i === activeIdx) {
+          gsap.set(s, { xPercent: 0 });
+          gsap.set(s.children[0], { xPercent: ((window.innerWidth - s.children[0]?.clientWidth) / (2 * s.children[0]?.clientWidth)) * 100  });
+        } else {
+          if (s === prev) {
+            gsap.set(s, { xPercent: -100 });
+          } else {
+            if (s === next) {
+              gsap.set(s, { xPercent: 95 });
+            } else {
+              gsap.set(s, { xPercent: 100 });
+              gsap.set(s.children[0], { xPercent: 0});
+            }
+          }
+        }
+      });
+    }, time);
+  };
+
+  goToNextSlide() {
+    const activeSlide = this.slider.querySelector('.active');
+    const activeIdx = this.slides.indexOf(activeSlide);
+    const [next, prev, nextAfterNext] = this.getNextPrev();
+
+    this.slides.forEach((s, i ) => {
+      if (i === activeIdx) {
+        gsap.to(s,  {xPercent: -100, duration: 0.8, ease: 'ease' });
       } else {
         if (s === next) {
-          gsap.set(s, { xPercent: 95 });
+          gsap.to(s, { xPercent: 0, duration: 0.8, ease: Power2.easeOut });
+          gsap.to(s.children[0],  { xPercent: ((window.innerWidth - s.children[0]?.clientWidth) / (2 * s.children[0]?.clientWidth)) * 100  });
         } else {
-          gsap.set(s, { xPercent: 100 });
-          gsap.set(s.children[0], { xPercent: 0});
+          if(s === nextAfterNext) {
+            gsap.to(s,  { xPercent: 95, duration: 0.65, ease: 'ease', delay: 0.45 });
+          } else {
+            gsap.set(s, { xPercent: 100 });
+            gsap.set(s.children[0], { xPercent: 0});
+          }
         }
       }
-    }
-  });
-};
-
-setTimeout( setInitialSliderPosition, 0);
-
-function goToNextSlide() {
-  const activeSlide = document.querySelector('.slide.active');
-  const [next, prev, nextAfterNext] = getNextPrev();
-  const activeIdx = slides.indexOf(activeSlide);
-
-  slides.forEach((s, i ) => {
-    if (i === activeIdx) {
-      gsap.to(s,  {xPercent: -100, duration: 0.8, ease: 'ease' });
-    } else {
-      if (s === next) {
-        gsap.to(s, { xPercent: 0, duration: 0.8, ease: Power2.easeOut });
-        gsap.to(s.children[0],  { xPercent: ((window.innerWidth - s.children[0]?.clientWidth) / (2 * s.children[0]?.clientWidth)) * 100  });
-      } else {
-        if(s === nextAfterNext) {
-          gsap.to(s,  { xPercent: 95, duration: 0.65, ease: 'ease', delay: 0.45 });
-        } else {
-          gsap.set(s, { xPercent: 100 });
-          gsap.set(s.children[0], { xPercent: 0});
-        }
-      }
-    }
-  });
-  activeSlide.classList.remove('active');
-  next.classList.add('active');
-};
-
-slides.forEach((s, i ) => {
-  s.addEventListener('click', () => onSlideClick(s, i));
-});
-
-const onSlideClick = (s, i) => {
-  const activeSlide = document.querySelector('.slide.active');
-  const activeIdx = slides.indexOf(activeSlide);
-
-  const [next, prev, nextAfterNext] = getNextPrev();
-  if (next === s || i ===  slides.length - 1) {
-    goToNextSlide();
+    });
+    activeSlide.classList.remove('active');
+    next.classList.add('active');
   }
-};
 
-function goToPrevSlide() {
-  const activeSlide = document.querySelector('.slide.active');
-  const [next, prev] = getNextPrev();
-  const activeIdx = slides.indexOf(activeSlide);
+  goToPrevSlide() {
+    const activeSlide = this.slider.querySelector('.active');
+    const activeIdx = this.slides.indexOf(activeSlide);
+    const [next, prev, nextAfterNext] = this.getNextPrev();
 
-  slides.forEach((s, i) => {
-    if (i === activeIdx) {
-      gsap.to(s,  {xPercent: 95, duration: 0.7, ease: Power2.easeOut});
-      gsap.to(s.children[0], {xPercent: 0});
-    } else {
-      if(s === prev) {
-        gsap.fromTo(s,  {xPercent: -100 }, { xPercent: 0, duration: 0.7, ease: Power2.easeOut });
-        gsap.to(s.children[0], { xPercent: ((window.innerWidth - s.children[0]?.clientWidth) / (2 * s.children[0]?.clientWidth)) * 100  });
+    this.slides.forEach((s, i) => {
+      if (i === activeIdx) {
+        gsap.to(s,  {xPercent: 95, duration: 0.7, ease: Power2.easeOut});
+        gsap.to(s.children[0], {xPercent: 0});
       } else {
-        if (s === next) {
-          gsap.to(s,  { xPercent: 100, duration: 0.3, ease: 'ease' });
+        if(s === prev) {
+          gsap.fromTo(s,  {xPercent: -100 }, { xPercent: 0, duration: 0.7, ease: Power2.easeOut });
+          gsap.to(s.children[0], { xPercent: ((window.innerWidth - s.children[0]?.clientWidth) / (2 * s.children[0]?.clientWidth)) * 100  });
         } else {
-          gsap.set(s, { xPercent: 100 });
-          gsap.set(s.children[0], { xPercent: 0});
+          if (s === next) {
+            gsap.to(s,  { xPercent: 100, duration: 0.3, ease: 'ease' });
+          } else {
+            gsap.set(s, { xPercent: 100 });
+            gsap.set(s.children[0], { xPercent: 0});
+          }
         }
       }
+    });
+    activeSlide.classList.remove('active');
+    prev.classList.add('active');
+  }
+
+  onSlideClick (s, i) {
+    const [next, prev, nextAfterNext] = this.getNextPrev();
+    if (next === s || i ===  this.slides.length - 1) {
+      this.goToNextSlide();
     }
-  });
-  activeSlide.classList.remove('active');
-  prev.classList.add('active');
+  };
 }
 
-const throttleClickNext = throttle(() => {
-  goToNextSlide();
-}, 200);
+new Slider(0);
 
-const throttleClickPrev = throttle(() => {
-  goToPrevSlide();
-}, 200);
-
-const nextBtn = document.querySelector('[data-slider="next"]');
-const prevBtn = document.querySelector('[data-slider="prev"]');
-
-nextBtn.addEventListener('click', () => {
-  throttleClickNext();
-});
-
-prevBtn.addEventListener('click', () => {
-  throttleClickPrev();
-});
-
-window.addEventListener('keydown', e => {
-  if(e.key === 'ArrowLeft') throttleClickPrev();
-  if(e.key === 'ArrowRight') throttleClickNext();
-});
