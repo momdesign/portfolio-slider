12e4528835868f884aa57da198ea4b54e0b5c8f5
sergokiko <sergokiko@gmail.com>
2021-07-15 16:56:57 +0300

    refactor Handle details, svg icons, and escape event

--- js/index.js
+++ js/index.js
@@ -10,31 +10,19 @@ const details = dqs('#more');
 const moreDetailsBtn = dqs('[data-animate="info"]');
 const closeBtn = dqs('[data-animate="close"]');
 const sliders = dqsa('.slider');
+const copyBtn = dqs('[data-animate="copy"]');
+const header = dqs('[data-animate="header"]');
 let isDetailsClose = true;
+let slidersArr = [];
 
 circularScroll.render();
-let slidersArr = [];
 
 sliders.forEach((_, i) => slidersArr.push(new Slider(i)));
 
-const handleDetails = () => {
-  const copyBtn = dqs('[data-animate="copy"]');
-  const arrows = dqsa('[data-animate="arrows"]');
-  const info = dqs('[data-more="info"]');
-  const name = dqs('[data-more="name"]');
-  const logo = dqs('[data-animate="logo"]');
-  const header = dqs('[data-animate="header"]');
-  const currentSlider = dqsa('.slider')[circularScroll.activeProject.index];
+const toggleSliderAndNavigation = (currentSlider) => {
   const slides = currentSlider.querySelectorAll('.slide');
   const sliderNavigation = dqsa('[data-navigation]');
-
-  isDetailsClose = !isDetailsClose;
-  circularScroll.setIsScrolling(isDetailsClose);
-  copyBtn.classList.toggle('more__copy-visible');
-  moreDetailsBtn.classList.toggle('more__icon-info-visible');
-  closeBtn.classList.toggle('more__icon-close--visible');
-  header.classList.toggle('more__header-visible');
-  logo.classList.toggle('logo-hidden');
+  const arrows = dqsa('[data-animate="arrows"]');
 
   circularScroll.scrollToProject().then(() => {
     slides.forEach(sl => {
@@ -45,28 +33,53 @@ const handleDetails = () => {
 
     arrows.forEach(el => el.classList.toggle('slider__arrows-visible'));
   });
-  circularScroll.descriptionHandler(isDetailsClose);
-  header.innerHTML = circularScroll.activeProject.name;
+  sliderNavigation.forEach(el => el.classList.toggle('slider-clicker-left-active'));
+  slidersArr[circularScroll.activeProject.index].isSliderOpen = !slidersArr[circularScroll.activeProject.index].isSliderOpen;
+}
+
+const toggleDescriptionClasses = () => {
+  const logo = dqs('[data-animate="logo"]');
+  const name = dqs('[data-more="name"]');
+
+  copyBtn.classList.toggle('more__copy-visible');
+  moreDetailsBtn.classList.toggle('more__icon-info-visible');
+  closeBtn.classList.toggle('more__icon-close--visible');
+  header.classList.toggle('more__header-visible');
+  logo.classList.toggle('logo-hidden');
 
   !isDetailsClose
     ? name.classList.toggle('more__name-hidden')
     : setTimeout(() => name.classList.toggle('more__name-hidden'), 250);
+}
+
+const onEscapePress = (e) => {
+  if(e.key === 'Escape') handleDetails();
+}
+
+const escapeEventHandler = () => {
+  isDetailsClose
+    ? window.addEventListener('keydown', onEscapePress)
+    : window.removeEventListener('keydown', onEscapePress)
+};
+
+const handleDetails = () => {
+  const info = dqs('[data-more="info"]');
+  const currentSlider = dqsa('.slider')[circularScroll.activeProject.index];
+
+  escapeEventHandler();
+  toggleSliderAndNavigation(currentSlider);
+  isDetailsClose = !isDetailsClose;
+  circularScroll.setIsScrolling(isDetailsClose);
+  toggleDescriptionClasses();
+  circularScroll.descriptionHandler(isDetailsClose);
+
+  header.innerHTML = circularScroll.activeProject.name;
 
   setTimeout(() => {
     info.innerHTML = !isDetailsClose ? circularScroll.activeProject.description : circularScroll.activeProject.info;
   }, 200);
 
   copyBtn.setAttribute('data-copy', circularScroll.copyLink);
-
-  slides.forEach(sl => {
-    if(!sl.classList.contains('active')){
-      sl.classList.toggle('slide-hidden');
-    }
-  });
-
-  sliderNavigation.forEach(el => el.classList.toggle('slider-clicker-left-active'));
-  arrows.forEach(el => el.classList.toggle('slider__arrows-visible'));
-  slidersArr[circularScroll.activeProject.index].isSliderOpen = !slidersArr[circularScroll.activeProject.index].isSliderOpen;
 };
 
 setTimeout(() => {
