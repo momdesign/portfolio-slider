83d97d92bd1edddb4bdf7306bf1588dc51452e7e
sergokiko <sergokiko@gmail.com>
2021-07-17 20:40:01 +0300

    intro animation

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-import gsap from 'gsap';
+import gsap, {Power2} from 'gsap';
 
 import CircularScroll from './circular';
 import Slider from './slider';
 import { closeContacts, openContacts } from './contact';
+import { initIntroAnimation } from './introAnimation'
+import { toggleDescriptionClasses } from './ toggleDescriptionClasses'
 
 export const circularScroll = new CircularScroll();
 
@@ -10,33 +12,13 @@ const dqs = document.querySelector.bind(document);
 const dqsa = document.querySelectorAll.bind(document);
 
 const details = dqs('#more');
-const moreDetailsBtn = dqs('[data-animate="info"]');
-const closeBtn = dqs('[data-animate="close"]');
 const sliders = dqsa('.slider');
 const copyBtn = dqs('[data-animate="copy"]');
 const header = dqs('[data-animate="header"]');
 let isDetailsClose = true;
 let slidersArr = [];
-let isIntroPlayed = false;
 
-
-document.querySelector('[data-contact="open-button"]')?.addEventListener('click', () => {
-  openContacts(() => circularScroll.toggleScrollingHandler(true));
-});
-document.querySelector('[data-contact="close-button"]')?.addEventListener('click', () => {
-  closeContacts(() => circularScroll.toggleScrollingHandler(false));
-});
-
-// comment this for intro
-circularScroll.startScrolling();
-// window.addEventListener('wheel', e => {
-//   if (isIntroPlayed || e.deltaY < 0) return;
-//
-//   isIntroPlayed = true;
-//   gsap.fromTo(sliders[0], { y: -250 }, { y: 0 }).then( () => {
-//     circularScroll.startScrolling();
-//   });
-// });
+initIntroAnimation(() => circularScroll.startScrolling());
 
 sliders.forEach((_, i) => slidersArr.push(new Slider(i)));
 
@@ -61,34 +43,6 @@ const toggleSliderAndNavigation = (currentSlider, time) => {
   slidersArr[circularScroll.activeProject.index].isSliderOpen = !slidersArr[circularScroll.activeProject.index].isSliderOpen;
 }
 
-const toggleDescriptionClasses = (time) => {
-  const logo = dqs('[data-animate="logo"]');
-  const name = dqs('[data-more="name"]');
-
-  copyBtn.classList.toggle('more__copy-visible');
-  moreDetailsBtn.classList.toggle('more__icon-info-visible');
-  closeBtn.classList.toggle('more__icon-close--visible');
-
-  setTimeout(() => {
-    header.classList.toggle('more__header-visible');
-    logo.classList.toggle('logo-hidden');
-  }, time + 200);
-
-  !isDetailsClose
-    ? name.classList.toggle('more__name-hidden')
-    : setTimeout(() => name.classList.toggle('more__name-hidden'), 250);
-}
-
-const onEscapePress = (e) => {
-  if(e.key === 'Escape') handleDetails();
-}
-
-const escapeEventHandler = () => {
-  isDetailsClose
-    ? window.addEventListener('keydown', onEscapePress)
-    : window.removeEventListener('keydown', onEscapePress)
-};
-
 const handleDetails = () => {
   const info = dqs('[data-more="info"]');
   const currentSlider = dqsa('.slider')[circularScroll.activeProject.index];
@@ -98,7 +52,7 @@ const handleDetails = () => {
   toggleSliderAndNavigation(currentSlider, delayTime);
   isDetailsClose = !isDetailsClose;
   circularScroll.setIsScrolling(isDetailsClose);
-  toggleDescriptionClasses(delayTime);
+  toggleDescriptionClasses(delayTime, isDetailsClose);
   circularScroll.toggleScrollingHandler(!isDetailsClose);
 
   header.innerHTML = circularScroll.activeProject.name;
@@ -110,8 +64,21 @@ const handleDetails = () => {
   copyBtn.setAttribute('data-copy', circularScroll.copyLink);
 };
 
-setTimeout(() => {
-  document.querySelector('[data-slider="first"]').classList.remove('slide-invisible');
-}, 300);
+const onEscapePress = (e) => {
+  if(e.key === 'Escape') handleDetails();
+}
+
+const escapeEventHandler = () => {
+  isDetailsClose
+    ? window.addEventListener('keydown', onEscapePress)
+    : window.removeEventListener('keydown', onEscapePress)
+};
 
 details.addEventListener('click', handleDetails);
+
+document.querySelector('[data-contact="open-button"]')?.addEventListener('click', () => {
+  openContacts(() => circularScroll.toggleScrollingHandler(true));
+});
+document.querySelector('[data-contact="close-button"]')?.addEventListener('click', () => {
+  closeContacts(() => circularScroll.toggleScrollingHandler(false));
+});
