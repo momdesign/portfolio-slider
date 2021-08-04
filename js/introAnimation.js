3a392f1c297c07706160eaac42bc209f08c471ab
sergokiko <sergokiko@gmail.com>
2021-08-04 12:21:21 +0300

    add mobile block and data attributes. throttle for scroll

--- js/introAnimation.js
+++ js/introAnimation.js
@@ -1,31 +1,92 @@
-import gsap, {Power2} from "gsap";
+import gsap, {Power2} from 'gsap';
 
 const dqs = document.querySelector.bind(document);
-const dqsa = document.querySelectorAll.bind(document);
 
-const sliders = dqsa('.slider');
-const logo = dqs('[data-animate="logo"]');
-const introLogo = dqs('[data-animate="intro-logo"]');
-let isIntroPlayed = false;
+class IntroAnimation {
+  constructor() {
+    this.introLogo = dqs('[data-animate="intro-logo"]');
+    this.xDown = 0;
+    this.yDown = 0;
+    this.xDiff = 0;
+    this.yDiff = 0;
+    this.isIntroStarted = false;
+    this.tlNyc = gsap.timeline({defaults:{ease:Power2.easeInOut}, paused: true})
+      .to('[data-logo-nyc]', {
+        duration: 1,
+        y: - window.innerHeight * 0.45,
 
-export const initIntroAnimation = onInit => {
-  const tl = gsap.timeline({ paused: true })
-    .from(sliders[0], { yPercent: 70, duration: 1.2, delay: 0.15, ease:Power2.easeInOut })
-  const  bigLogoAnimation = gsap.from(introLogo, {
-      y: window.innerHeight / 2 + window.innerWidth / 20,
-      height: '10vw',
-      duration: 1.2,
-      ease:Power2.easeInOut}).paused(true);
-  const smallLogoAnimation = gsap.from(logo, {y: - window.innerWidth / 10, opacity: 0, delay: 0.4}).paused(true);
+      }, 0)
+      .to('[data-logo-nyc]', {
+        opacity: 0,
+        duration: 0.4,
+        delay: -0.8
+      });
+    this.tlLogo = gsap.timeline({defaults:{ease:Power2.easeInOut}, paused: true})
+      .to('svg', {
+        scale: 0.3,
+        duration: 0.3,
+        y: - window.innerHeight * 0.45,
+      }, 0)
+      .fromTo('svg [id="M2"], svg [id="O"], svg [id="E"], svg [id="S"], svg [id="I"], svg [id="G"], svg [id="N"]', {
+        opacity: 1}, {opacity: 0, duration: 0.1}, 0.13)
+      .fromTo('svg [id="N-small"], svg [id="Y"], svg [id="C"]', {
+        opacity: 0}, {opacity: 1, duration: 0.15}, 0.12)
+      .to('svg [id="M"]', {xPercent: "+165", duration: 0.15}, 0.12)
+      .to('svg [id="D"]', {xPercent: "-40", duration: 0.15}, 0.12);
+    this.projectInfoAnimation = gsap.fromTo('[data-more="more"]', {opacity: 0}, {opacity: 1, duration: 1, delay: 0.8}).paused(true);
+  }
 
-  window.addEventListener('wheel', e => {
-    if (isIntroPlayed || e.deltaY < 0) return;
-    isIntroPlayed = true;
-    bigLogoAnimation.play();
-    smallLogoAnimation.play();
+  static getTouches(evt) {
+    return evt.touches || evt.originalEvent.touches;
+  }
 
-    tl.play().then( () => {
-      onInit();
+  onTouchStart(evt) {
+    const firstTouch = IntroAnimation.getTouches(evt)[0];
+    this.xDown = firstTouch.clientX;
+    this.yDown = firstTouch.clientY;
+  }
+
+  onTouchEnd(cb) {
+    return () => {
+      if (Math.abs(this.xDiff) < Math.abs(this.yDiff) && this.yDiff > 80 && !this.isIntroStarted) {
+        cb();
+      }
+
+      this.xDown = null;
+      this.yDown = null;
+      this.xDiff = null;
+      this.yDiff = null;
+    };
+  }
+
+  onTouchMove(evt) {
+    if ( !this.xDown || !this.yDown ) {
+      return;
+    }
+
+    this.xDiff = this.xDown - evt.touches[0].clientX;
+    this.yDiff = this.yDown - evt.touches[0].clientY;
+  }
+
+  start() {
+    this.isIntroStarted = true;
+
+    this.tlLogo.timeScale(0.3)
+    this.tlLogo.play();
+    this.tlNyc.play();
+    this.projectInfoAnimation.play();
+  }
+
+  init(cb) {
+    window.addEventListener('wheel', e => {
+      if (this.isIntroStarted || e.deltaY < 0) return;
+      this.start();
+      cb();
     });
-  });
-};
+    window.addEventListener('touchstart', this.onTouchStart);
+    window.addEventListener('touchmove', this.onTouchMove);
+    window.addEventListener('touchend', this.onTouchEnd(this.start));
+  }
+}
+
+export default IntroAnimation;
--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,48 @@
-window.addEventListener('wheel', (e) => {
+import IntroAnimation from "./introAnimation";
+import throttle from './utils/throttle';
 
+const introAnimation = new IntroAnimation();
+
+introAnimation.init(() => goToNextProject);
+
+const projectsData = [
+  {
+    background: '#1f285f',
+    name: 'SIMON OSTROVSKY',
+    info: 'portfolio website',
+    service: 'design / web',
+    year: '2021'
+  },
+  {
+    background: '#ffffff',
+    name: 'Liminal',
+    info: '',
+    service: 'Design / Development',
+    year: 'WIP'
+  }
+]
+
+const projectsBlock = document.querySelector('.projects');
+const projectItems = document.querySelectorAll('.projects__img--mask');
+let index = 0;
+
+const goToNextProject = () => {
+  projectsBlock.style.backgroundColor = projectsData[index]?.background;
+  projectItems.forEach(el => {
+    setTimeout(() => {
+      el.style.transform = `translateY(${100 - index * 100}%)`;
+    },  1000 + 1000 * Math.random())
+  });
+  index++;
+};
+
+const throttleGoToNextProject = throttle( () => goToNextProject() ,  4000 );
+
+window.addEventListener('wheel', () => {
+
+  if(index){
+    throttleGoToNextProject();
+  }else {
+    goToNextProject()
+  }
 });
